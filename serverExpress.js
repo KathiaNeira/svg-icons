31b7cf45685f72f1c1f055502f9f80b71f7101a1
kathia <kathia.neira@orbis.com.pe>
2017-06-02 17:20:13 -0500

    fix(rutas):bug rutas relativas

--- serverExpress.js
+++ serverExpress.js
@@ -6,18 +6,6 @@ app.get('/',function(request, response){
   response.sendFile(__dirname + '/index.html');
 });
 
-app.get('/page1',function(request, response){
-  response.sendFile(__dirname + '/page1.html');
-});
-
-app.get('/page2',function(request, response){
-  response.sendFile(__dirname + '/page2.html');
-});
-
-app.get('/page3',function(request, response){
-  response.sendFile(__dirname + '/page3.html');
-});
-
 app.listen(3000, function(){
   console.log('Server Express Ready!');
 });
